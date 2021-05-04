c43033f33bb045b7f6c826bb996a458709dc1f7d
AndrewPochapsky <andrew.pochapsky@gmail.com>
2021-05-04 18:40:12 -0400

    added time of purchase

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from functools import lru_cache
 import csv
 import requests
+import pytz
+from datetime import datetime
 
 
 def get_orders(last_transaction_id: int, address: str) -> list:
@@ -18,13 +20,24 @@ def get_orders(last_transaction_id: int, address: str) -> list:
         if not is_buy_order:
             continue
         # Disregarding time for now.
-        date = order["timestamp"].split("T")[0]
+        date = convert_utc_to_est(order["timestamp"].split(".")[0])
+        coin_gecko_date = order["timestamp"].split("T")[0]
         eth_amount = int(order["amount_sold"]) / wei_amount
         card_name = order["buy"]["data"]["properties"]["name"]
-        eth_price = get_eth_price("-".join(date.split("-")[::-1]))
+        eth_price = get_eth_price("-".join(coin_gecko_date.split("-")[::-1]))
         orders.append([date, card_name, eth_amount, eth_price])
 
     return orders
+
+
+def convert_utc_to_est(utc_datetime) -> str:
+    est = pytz.timezone('US/Eastern')
+    utc = pytz.utc
+    fmt = '%m/%d/%Y %H:%M:%S'
+    d = datetime.strptime(utc_datetime, "%Y-%m-%dT%H:%M:%S")
+    d = datetime(d.year, d.month, d.day, d.hour,
+                 d.minute, d.second, tzinfo=utc)
+    return str(d.astimezone(utc).astimezone(est).strftime(fmt))
 
 
 @lru_cache
@@ -37,7 +50,8 @@ def get_eth_price(date: str) -> int:
 def format_csv(orders: list):
     def format_order(order):
         date, card_name, eth_amount, eth_price = order
-        return [date, "imx buy", "$" + str(eth_price), 0, 0, card_name, "M", eth_amount]
+        return [date, "imx buy", "$" + str(eth_price), 0,
+                0, card_name, "M", eth_amount]
     rows = list(map(format_order, orders))
     return rows
 
